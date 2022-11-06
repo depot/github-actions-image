250991f4423dc785895a53b1bfa582ee563a6865
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-11-06 16:05:27 +0000

    Add missing group, set default user for cloud-init

--- scripts/provision-user.sh
+++ scripts/provision-user.sh
@@ -4,6 +4,7 @@
 
 user=runner
 useradd --home-dir "/home/$user" --create-home --gid docker --uid 1001 --groups adm,systemd-journal --shell /bin/bash "$user"
+groupadd --gid 1001 runner
 echo "runner ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/runner
 echo "RUNNER_USER=$user" | tee -a /etc/environment
 user_id=$(id -u $user)
@@ -11,3 +12,9 @@ echo "d /run/user/$user_id 0700 $user $user" >> /tmp/1001-users.conf
 systemd-tmpfiles --create /tmp/1001-users.conf
 echo "XDG_RUNTIME_DIR=/run/user/$user_id" | tee -a /etc/environment
 rm /tmp/1001-users.conf
+
+cat <<EOF > /etc/cloud/cloud.cfg.d/00_defaults.cfg
+system_info:
+  default_user:
+    name: runner
+EOF
